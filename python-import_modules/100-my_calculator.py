bacbd6603d86ce313600bcca0f128c9dfb964d5e
s-chukwu <s.chukwu@alustudent.com>
2026-02-26 11:11:56 -0800

    build my own calculator!

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    # Map operators to their respective functions
+    ops = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if operator not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    result = ops[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
