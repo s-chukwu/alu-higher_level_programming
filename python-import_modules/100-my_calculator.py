a12cf899dfe6a89b82a95188cbf287a8be1e64e8
s-chukwu <s.chukwu@alustudent.com>
2026-02-26 11:18:36 -0800

    build my own calculator!

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,29 +3,29 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    # Validate the number of arguments
+    # Check if the number of arguments is exactly 3 (plus the script name itself)
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    # Assigned arguments to variables
+    # Assign arguments to variables
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    # Handle operations
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
-    else:
+    # Dictionary to map operators to their respective functions
+    ops = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    # Check if the operator is valid
+    if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    # Print the result
+    # Perform the calculation and print the result
+    result = ops[operator](a, b)
     print("{} {} {} = {}".format(a, operator, b, result))
-
