33db4e298cdfedbf2ff5d7f7c29280c610282401
s-chukwu <s.chukwu@alustudent.com>
2026-02-26 11:25:59 -0800

    fix build my own calculator!

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 import sys
-import calculator_1
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    # Check if the number of arguments is exactly 3
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
     operator = sys.argv[2]
-    b = int(sys.argv[3])
 
-    # Dictionary mapping operators to the functions inside calculator_1
+    # Map the operators directly to the explicitly imported functions
     ops = {
-        "+": calculator_1.add,
-        "-": calculator_1.sub,
-        "*": calculator_1.mul,
-        "/": calculator_1.div
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
     }
 
-    # Check for valid operator
     if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    # Perform calculation and print
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    # Call the correct function from the dictionary
     result = ops[operator](a, b)
+
     print("{} {} {} = {}".format(a, operator, b, result))
