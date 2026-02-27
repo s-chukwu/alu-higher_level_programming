9157ef493db269879f0753da60686ac2a8850e9a
s-chukwu <s.chukwu@alustudent.com>
2026-02-27 08:11:38 -0800

    fixed calc

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-"""Module that handles basic arithmetic operations using calculator_1 functions."""
+"""Handles basic operations using calculator_1 functions."""
 import sys
 from calculator_1 import add, sub, mul, div
 
 
 def main():
-    """Main function to handle calculator operations."""
+    """Entry point for the calculator script."""
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a = int(sys.argv[1])
-    operator = sys.argv[2]
+    op = sys.argv[2]
     b = int(sys.argv[3])
 
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == chr(42):
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    ops = ['+', '-', chr(42), '/']
+    if op not in ops:
+        print("Unknown operator. Available operators: +, -, " +
+              chr(42) + " and /")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+    if op == '+':
+        result = add(a, b)
+    elif op == '-':
+        result = sub(a, b)
+    elif op == chr(42):
+        result = mul(a, b)
+    else:
+        result = div(a, b)
 
-
-if __name__ == "__main__":
-    main()
-
+    print("{} {} {} = {}".format(a, op, b, result))
 
 
 if __name__ == "__main__":
