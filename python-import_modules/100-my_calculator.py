e7661f6f0a85ac14c041655faee48b947bfc9b59
s-chukwu <s.chukwu@alustudent.com>
2026-02-27 08:03:36 -0800

    fixed calc

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
+"""Module that handles basic arithmetic operations using calculator_1 functions."""
 import sys
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
+
+def main():
+    """Main function to handle calculator operations."""
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    a = int(sys.argv[1])
     operator = sys.argv[2]
-    if operator not in ["+", "-", "*", "/"]:
+    b = int(sys.argv[3])
+
+    operators = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+
+    if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    result = operators[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
 
-    if operator == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    main()
