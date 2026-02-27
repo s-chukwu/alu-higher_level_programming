5097c8fa4dcfb1253d8fa7cc2cf781423f361376
s-chukwu <s.chukwu@alustudent.com>
2026-02-27 08:15:58 -0800

    fixed calc

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
-"""Handles basic operations using calculator_1 functions."""
+"""Calculator script using calculator_1 functions."""
 import sys
 from calculator_1 import add, sub, mul, div
 
 
-def main():
-    """Entry point for the calculator script."""
+if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
-
-    ops = ['+', '-', chr(42), '/']
-    if op not in ops:
-        print("Unknown operator. Available operators: +, -, " +
-              chr(42) + " and /")
-        sys.exit(1)
-
     if op == '+':
         result = add(a, b)
     elif op == '-':
         result = sub(a, b)
     elif op == chr(42):
         result = mul(a, b)
-    else:
+    elif op == '/':
         result = div(a, b)
-
+    else:
+        print("Unknown operator. Available operators: +, -, " + chr(42) + " and /")
+        sys.exit(1)
     print("{} {} {} = {}".format(a, op, b, result))
-
-
-if __name__ == "__main__":
-    main()
