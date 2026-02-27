87d06f4a82edfdfc9494a4ba9cc29ae5a4f53547
s-chukwu <s.chukwu@alustudent.com>
2026-02-27 07:58:00 -0800

    fixed calc

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,25 +2,24 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     operator = sys.argv[2]
-    if operator not in ['+', '-', '*', '/']:
+    if operator not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if operator == '+':
+    if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == '-':
+    elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == '*':
+    elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == '/':
+    elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
