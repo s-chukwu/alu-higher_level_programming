3531bf69f8b194f36ed5bfdf10cd1ebbf4ae3fe7
s-chukwu <s.chukwu@alustudent.com>
2026-03-22 11:22:42 -0700

    ByteCode

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    if c > b:
-        return a + b
-    return a * b - c
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except Exception:
+            result = b + a
+            break
+    return result
