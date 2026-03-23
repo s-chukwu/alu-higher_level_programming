6bfd202458ff7367dc883734afdbbbf2f69b5c03
s-chukwu <s.chukwu@alustudent.com>
2026-03-23 15:16:01 -0700

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Class Square that defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
